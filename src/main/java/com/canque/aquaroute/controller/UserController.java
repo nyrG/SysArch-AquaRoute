a46d1b9bac29eb6afcaa014ab209b61fa8a7948b
Ymiel Canque <104157553+nyrG@users.noreply.github.com>
2024-07-25 23:13:20 +0800

    Cross Origin update
    Added @CrossOrigin annotation to controllers

--- src/main/java/com/canque/aquaroute/controller/UserController.java
+++ src/main/java/com/canque/aquaroute/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ import com.canque.aquaroute.repository.UserRepository;
 
 import springfox.documentation.annotations.ApiIgnore;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class UserController {
 
